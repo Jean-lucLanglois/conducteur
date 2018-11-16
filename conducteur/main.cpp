9b967c0321820906823ba968ebdb9f554b8ce184
Jean-Luc <miamisp@mac.com>
2018-11-16 10:32:04 -1000

    connection string update

--- conducteur/main.cpp
+++ conducteur/main.cpp
@@ -15,9 +15,55 @@
 #include <errno.h>
 
 
+// Function to scoop stocks
+bool scoop_stocks (){
+    
+    // Let's start populating records in the queue for the looper slaves
+    const std::string CONN_STRING = "user=instigator_app  host=jeanluc-db.c0hxc3wgxzra.us-east-1.rds.amazonaws.com  password=zfszsT38ED  dbname=instigator";
+    std::string scoop_stocks ="UPDATE stock_symbols set scoop = true";
+    pqxx::connection C{CONN_STRING};
+    pqxx::work W{C};
+    try {
+        W.exec(scoop_stocks);
+        W.commit();
+    } catch (const std::exception& e) {
+        W.abort();
+        std::cerr << e.what();
+        return false;
+    }
+    return true;
+}
+
+// Function to update App Preferences
+bool set_app_preferences(bool &engine_status, int &pull_interval, int &stock_count){
+    
+    const std::string CONN_STRING = "user=instigator_app  host=jeanluc-db.c0hxc3wgxzra.us-east-1.rds.amazonaws.com  password=zfszsT38ED  dbname=instigator";
+    std::string query_app_preferences ="SELECT engine_status, stock_count, pull_interval FROM app_preferences";
+    pqxx::connection C{CONN_STRING};
+    pqxx::work W{C};
+    try {
+        pqxx::result R = W.exec(query_app_preferences);
+        W.commit();
+    
+        // Let's SET engine_status, pull_interval, stock_count from db
+        engine_status = R[0]["engine_status"].as<bool>();
+        pull_interval = R[0]["pull_interval"].as<int>();
+        stock_count = R[0]["stock_count"].as<int>();
+        std::cout << "CONDUCTEUR::Pulling App Preferences:" << std::endl;
+        std::cout << "CONDUCTEUR::Engine Status:" << engine_status << std::endl;
+        std::cout << "CONDUCTEUR::Pull Interval:" << pull_interval << std::endl;
+        std::cout << "CONDUCTEUR::Stock Count:" << stock_count << std::endl;
+    } catch (const std::exception& e) {
+        W.abort();
+        std::cerr << e.what();
+        return false;
+    }
+    return true;
+}
 
 int main(int argc, const char * argv[]) {
     
+    // Set constants and variables
     const int RECORDS_PER_BATCH = 300  ;
     const std::string CONN_STRING = "user=instigator_app  host=jeanluc-db.c0hxc3wgxzra.us-east-1.rds.amazonaws.com  password=zfszsT38ED  dbname=instigator";
     bool engine_status = false;
@@ -25,70 +71,50 @@ int main(int argc, const char * argv[]) {
     int stock_count = 0;
     char * PID = (char *)getpid;
     
-    // Connect to DB and pull variables
-    // Establish DB Connection
-    std::string query_app_preferences ="SELECT engine_status, stock_count, pull_interval FROM app_preferences";
-    pqxx::connection C{CONN_STRING};
-    pqxx::work W{C};
-    pqxx::result R = W.exec(query_app_preferences);
-    W.commit();
-    
-    // Let's SET engine_status, pull_interval, stock_count from db
-    engine_status = R[0]["engine_status"].as<bool>();
-    pull_interval = R[0]["pull_interval"].as<int>();
-    stock_count = R[0]["stock_count"].as<int>();
-    std::cout << "CONDUCTEUR::Engine Status:" << engine_status << std::endl;
-    std::cout << "CONDUCTEUR::Pull Interval:" << pull_interval << std::endl;
-    std::cout << "CONDUCTEUR::Stock Count:" << stock_count << std::endl;
-    
+    // Connect to DB and pull app preferences
+    if(set_app_preferences(engine_status, pull_interval, stock_count)){
+        std::cout << "CONDUCTEUR::App Preferences Pulled!!" << std::endl;
+    }else{
+        std::cout << "CONDUCTEUR::ERROR::Unable to pull App Preferences!!" << std::endl;
+    }
     
     // Let's figure out how many looper to spawn
-    // make sure we have stocks to pull
-    if (stock_count > 0) {
+    if (stock_count > 0) {  // make sure we have stocks to pull
         int scooper_spawn_count = (stock_count / RECORDS_PER_BATCH) + 1;
         std::cout << "CONDUCTEUR::There is " << stock_count << " stock names to pull. " << std::endl;
         std::cout << "CONDUCTEUR::I'm going to spawn " << scooper_spawn_count << " scoopers to handle the load." << std::endl;
         
-        // spawn the scoopers
-        // Create argument array with PID for system call
-        char *args[] = {(char*)"/usr/local/bin/scooper", (char *)PID , (char *) 0 };
-//          std::string e = "/usr/local/bin/scooper " + std::to_string(PID);
-//          const char *  scooper_call = e.c_str();
-            // Lets loop through scooper_spawn_count
-            for (int i=1; i <= scooper_spawn_count; i++) {
-                pid_t child_pID = fork();
-                if (child_pID == 0)                // child
-                {
-                    // Function call does not return on success.
-                    errno = 0;
-                    int execReturn = execv("/usr/local/bin/scooper", args);
-                    if(execReturn == -1)  {
-                        std::cout << "CONDUCTEUR::Failure! execv error code=" << errno << std::endl;
-                    }
-                    if(execReturn == -1) {
-                        std::cout << "CONDUCTEUR::Failure! execv error code=" << errno << std::endl;
-                    }
-                    _exit(0); // If exec fails then exit forked process.
-                }
-                else if (child_pID < 0)  {           // failed to fork
-                    std::cerr << "CONDUCTEUR::Failed to fork" << std::endl;
-                }else{                         // parent
-                    std::cout << "CONDUCTEUR::Parent Process" << std::endl;
-                }
-                std::cout << "CONDUCTEUR::scooper " << i << " launched " << args[1] << std::endl;
-            }
+//        // spawn the scoopers
+//        // Create argument array with PID for system call
+//        char *args[] = {(char*)"/usr/local/bin/scooper", (char *)PID , (char *) 0 };
+////          std::string e = "/usr/local/bin/scooper " + std::to_string(PID);
+////          const char *  scooper_call = e.c_str();
+//            // Lets loop through scooper_spawn_count
+//            for (int i=1; i <= scooper_spawn_count; i++) {
+//                pid_t child_pID = fork();
+//                if (child_pID == 0)   {           // child
+//                    // Function call does not return on success.
+//                    errno = 0;
+//                    int execReturn = execv("/usr/local/bin/scooper", args);
+//                    if(execReturn == -1)  {
+//                        std::cout << "CONDUCTEUR::Failure! execv error code=" << errno << std::endl;
+//                    }
+//                    if(execReturn == -1) {
+//                        std::cout << "CONDUCTEUR::Failure! execv error code=" << errno << std::endl;
+//                    }
+//                    _exit(0); // If exec fails then exit forked process.
+//                }
+//                else if (child_pID < 0)  {           // failed to fork
+//                    std::cerr << "CONDUCTEUR::Failed to fork" << std::endl;
+//                }else{                         // parent
+//                    std::cout << "CONDUCTEUR::Parent Process" << std::endl;
+//                }
+//                std::cout << "CONDUCTEUR::scooper " << i << " launched " << args[1] << std::endl;
+//            }
         
         // Let's start the timer
         auto start_scooping = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now());
-        
-        // Let's start populating records in the queue for the looper slaves
-        std::string scoop_stocks ="UPDATE stock_symbols set scoop = true";
-        pqxx::connection C{CONN_STRING};
-        pqxx::work W{C};
-        W.exec(scoop_stocks);
-        W.commit();
-        
-        
+
         // Let's wait until engine turns on
         while(!engine_status){
             std::this_thread::sleep_for(std::chrono::milliseconds(30000));
@@ -117,20 +143,23 @@ int main(int argc, const char * argv[]) {
             std::this_thread::sleep_for(std::chrono::milliseconds(sleepy_time));
             
             // Let's enable records in the queue for the looper slaves
-//            pqxx::connection C{CONN_STRING};
-//            pqxx::work W{C}; 
-            W.exec(scoop_stocks);
-            W.commit();
+            // Let's start populating records in the queue for the looper slaves
+            if(scoop_stocks()){
+                std::cout << "CONDUCTEUR::Scooped stocks!!" << std::endl;
+            }else{
+                std::cout << "CONDUCTEUR::ERROR::Unable to scoop stocks!!" << std::endl;
+            }
     
             // Let's check if the engine is still on
             // Connect to DB and pull variables
             // Establish DB Connection
-            pqxx::result R = W.exec(query_app_preferences);
-            W.commit();
 
-            // Let's GET engine_status
-            engine_status = R[0]["engine_status"].as<bool>();
-            std::cout << "CONDUCTEUR::Engine Status:" << engine_status << std::endl;
+            // Connect to DB and pull app preferences
+            if(set_app_preferences(engine_status, pull_interval, stock_count)){
+                std::cout << "CONDUCTEUR::App Preferences Pulled!!" << std::endl;
+            }else{
+                std::cout << "CONDUCTEUR::ERROR::Unable to pull App Preferences!!" << std::endl;
+            }
         };
         
     }else{
